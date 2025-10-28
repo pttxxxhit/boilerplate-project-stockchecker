ce7e1d90bad3dc40fa969a76e977ea510e60bc4e
Ernesto Cristian Merino <ernestomerinocc@gmail.com>
2025-10-28 16:49:05 -0300

    Reto completado: stock checker con likes y comparación

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,6 +2,7 @@ const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
+const { suite, test } = require('mocha');
 
 chai.use(chaiHttp);
 
